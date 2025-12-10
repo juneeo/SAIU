ee06f1ebf2f237d990f3bee69c1f1c850ed2cd4f
nagorelarranaga <nagorelarranaga25@gmail.com>
2025-12-10 15:56:50 +0100

    Printzipala proba

---
+++ .vscode/src/Printzipala.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Printzipala {
+
+    public Printzipala() {
+        JFrame leihoa = new JFrame("SAIU");
+        leihoa.setLocation(500, 200);
+        leihoa.setSize(400, 600);
+
+        leihoa.setContentPane(panelaSortu());
+        leihoa.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        leihoa.setVisible(true);
+    }
+
+    private Container panelaSortu() {
+        JPanel panela = new JPanel(new BorderLayout());
+        panela.setBackground(Color.BLUE);   // fondo azul
+        return panela;
+    }
+
+    public static void main(String[] args) {
+        new Printzipala();
+    }
+}
